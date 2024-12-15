0455ad309708ccabcd15a327352ffeca39307d8a
Hieu <phamhieu280805@gmail.com>
2024-12-15 23:26:17 +0700

    Update transaction.sql

--- transaction.sql
+++ transaction.sql
@@ -1,20 +1,22 @@
+-- Bắt đầu giao dịch
 START TRANSACTION;
 
--- Thêm vé cho khách hàng vào bảng Ve
+-- Thêm một vé mới vào bảng Ve
 INSERT INTO Ve (MaLichChieu, MaGhe, MaKhachHang, ThoiGianDatVe)
 VALUES (1, 5, 2, NOW());
 
-UPDATE Ghe
-SET TrangThai = 'DaDat'
-WHERE MaGhe = 5 AND TrangThai = 'ConGhe';
+-- Tạo một điểm lưu sau khi thêm vé thành công
+SAVEPOINT SauKhiThemVe;
 
--- Kiểm tra nếu ghế đã bị đặt (Trường hợp lỗi: ví dụ ghế đã được đặt trước đó)
-IF ROW_COUNT() = 0 THEN
-    -- Nếu không có ghế nào được cập nhật, có thể ghế đã bị đặt trước đó, rollback giao dịch
-    ROLLBACK;
-    SELECT 'Lỗi: Ghế đã bị đặt trước đó, giao dịch bị hủy!';
-    LEAVE;
-END IF;
+-- Thử thêm một vé trùng (sẽ gây lỗi do ràng buộc UNIQUE trên MaLichChieu và MaGhe)
+INSERT INTO Ve (MaLichChieu, MaGhe, MaKhachHang, ThoiGianDatVe)
+VALUES (1, 5, 2, NOW());
 
+-- Nếu xảy ra lỗi, quay lại điểm lưu
+ROLLBACK TO SauKhiThemVe;
+
+-- Tiếp tục giao dịch và thêm một vé hợp lệ khác
+INSERT INTO Ve (MaLichChieu, MaGhe, MaKhachHang, ThoiGianDatVe)
+VALUES (1, 6, 3, NOW());
+-- Kết thúc và lưu giao dịch
 COMMIT;
-
